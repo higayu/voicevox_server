aaceba9165e6d1e1c4516752d5497f117d79d4d0
higashiyama <higashiyama2024fcc@gmail.com>
2025-08-27 18:50:50 +0900

    appのサーバー化成功

--- test.py
+++ test.py
@@ -1,30 +1,26 @@
-from fastapi import FastAPI, Query
-from fastapi.responses import FileResponse
-import tempfile
-from pathlib import Path
-
 from voicevox_core.blocking import Onnxruntime, OpenJtalk, Synthesizer, VoiceModelFile
 
-# パス設定
 onnxruntime_path = "/opt/voicevox_setup/voicevox_core/onnxruntime/lib/libvoicevox_onnxruntime.so.1.17.3"
 dict_dir = "/opt/voicevox_setup/voicevox_core/dict/open_jtalk_dic_utf_8-1.11"
 vvm_path = "/opt/voicevox_setup/voicevox_core/models/vvms/1.vvm"
 
-# 初期化
 onnxruntime = Onnxruntime.load_once()
 open_jtalk = OpenJtalk(dict_dir)
 synthesizer = Synthesizer(onnxruntime, open_jtalk)
 
-# モデル読み込み
 voice_model = VoiceModelFile.open(vvm_path)
+print("モデル情報:", voice_model.metas)
+
 synthesizer.load_voice_model(voice_model)
 
-# デフォルトのスタイルID
-style_id_default = voice_model.metas[0].styles[0].id
+# モデル情報から style_id を取得
+style_id = voice_model.metas[0].styles[0].id
+print("使う style_id:", style_id)
 
-# FastAPI インスタンス
-app = FastAPI()
+query = synthesizer.create_audio_query("おはようございます", style_id=style_id)
+wave = synthesizer.synthesis(query, style_id=style_id)
 
-@app.get("/speak")
-async def speak(text: str, style_id: int):
-    return {"text": text, "style_id": style_id}
+with open("test.wav", "wb") as f:
+    f.write(wave)
+
+print("✅ test.wav を出力しました！")
